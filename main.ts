b861b1bda757965c200c704495bec69fd871e4b4
Roj <ez@roj.im>
2023-07-27 16:56:39 +0300

    Remove unsupported API call and update MTKruto

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import {
   Client,
   httpTransportProvider,
-} from "https://deno.land/x/mtkruto@0.0.970/mod.ts";
+} from "https://deno.land/x/mtkruto@0.0.972/mod.ts";
 import {
   StorageDenoKV,
-} from "https://deno.land/x/mtkruto@0.0.970/storage/1_storage_deno_kv.ts";
+} from "https://deno.land/x/mtkruto@0.0.972/storage/1_storage_deno_kv.ts";
 import env from "./env.ts";
 
 const client = new Client(
@@ -21,5 +21,3 @@ await client.authorize(env.BOT_TOKEN);
 await client.sendMessage("@MTKrutoTest", "Hello from Deno Deploy!");
 
 await client.disconnect();
-
-Deno.exit();
