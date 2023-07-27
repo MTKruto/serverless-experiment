5eeb7525d94533348ca38b38630f0328001e1517
Roj <ez@roj.im>
2023-07-27 17:12:06 +0300

    Listen

--- main.ts
+++ main.ts
@@ -7,17 +7,24 @@ import {
 } from "https://deno.land/x/mtkruto@0.0.973/storage/1_storage_deno_kv.ts";
 import env from "./env.ts";
 
-const client = new Client(
-  new StorageDenoKV(),
-  env.API_ID,
-  env.API_HASH,
-  { transportProvider: httpTransportProvider({ initialDc: "1" }) },
-);
+Deno.serve(async () => {
+  try {
+    const client = new Client(
+      new StorageDenoKV(),
+      env.API_ID,
+      env.API_HASH,
+      { transportProvider: httpTransportProvider({ initialDc: "1" }) },
+    );
 
-await client.connect();
+    await client.connect();
 
-await client.authorize(env.BOT_TOKEN);
+    await client.authorize(env.BOT_TOKEN);
 
-await client.sendMessage("@MTKrutoTest", "Hello from Deno Deploy!");
+    await client.sendMessage("@MTKrutoTest", "Hello from Deno Deploy!");
 
-await client.disconnect();
+    await client.disconnect();
+    return new Response(null);
+  } catch (err) {
+    return new Response(Deno.inspect(err, { colors: false }), { status: 500 });
+  }
+});
